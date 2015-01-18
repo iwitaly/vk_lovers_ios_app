cdc95600c39b93190fdf46906602605201b4c35a
Vitaly Davydov <iwitaly@me.com>
2015-01-18 15:22:45 +0300

    Initial Commit

---
+++ VKLovers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKLovers
+//
+//  Created by Vitaly Davydov on 18/01/15.
+//  Copyright (c) 2015 Vitaly Davydov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKLoversTests/VKLoversTests.m
@@ -0,0 +1,40 @@
+//
+//  VKLoversTests.m
+//  VKLoversTests
+//
+//  Created by Vitaly Davydov on 18/01/15.
+//  Copyright (c) 2015 Vitaly Davydov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface VKLoversTests : XCTestCase
+
+@end
+
+@implementation VKLoversTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
